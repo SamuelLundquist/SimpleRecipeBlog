74c1add87c80773e68e94e6f54ed1f941fe17eac
Samuel Lundquist <50561985+SamuelLundquist@users.noreply.github.com>
2022-01-03 18:02:20 -0800

    Added dynamic latest recipe section

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,6 @@
 require('../models/database');
 const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
 
 
 /**
@@ -8,9 +9,12 @@ const Category = require('../models/Category');
 **/
 exports.homepage = async(req, res) => {
 	try {
-			const limitNumber = 5;
-			const categories = await Category.find({}).limit(limitNumber);
-			res.render('index', { title: 'Cooking Blog - Home', categories } );
+			const categoriesLimitNumber = 5;
+			const latestLimitNumber = 4;
+			const categories = await Category.find({}).limit(categoriesLimitNumber);
+			const latest = await Recipe.find({}).sort({_id: -1}).limit(latestLimitNumber);
+			const food = {latest};
+			res.render('index', { title: 'Cooking Blog - Home', categories, food } );
 	} catch (e) {
 		res.status(500).send({message: error.message || "Error occurred."});
 	}
@@ -31,3 +35,40 @@ exports.exploreCategories = async(req, res) => {
 	}
 
 }
+
+/*
+async function insertDummyRecipeData() {
+	try {
+		await Recipe.insertMany([
+			{
+				"name": "Recipe Name Goes Here",
+				"description": `Recipe Description Goes Here`,
+				"email": "recipeemail@raddy.co.uk",
+				"ingredients": [
+					"1 level teaspoon baking powder",
+					"1 level teaspoon cayenne pepper",
+					"1 level teaspoon hot smoked paprika",
+				],
+				"category": "American",
+				"image": "southern-friend-chicken.jpg"
+			},
+			{
+				"name": "Recipe Name Goes Here",
+				"description": `Recipe Description Goes Here`,
+				"email": "recipeemail@raddy.co.uk",
+				"ingredients": [
+					"1 level teaspoon baking powder",
+					"1 level teaspoon cayenne pepper",
+					"1 level teaspoon hot smoked paprika",
+				],
+				"category": "American",
+				"image": "southern-fried-chicken.jpg"
+			},
+		]);
+	} catch (e) {
+		console.log('Error:', + e);
+	}
+}
+
+insertDummyRecipeData();
+*/
